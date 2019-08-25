232d84002848c4443be0964366c9bc0cf4da4376
Nelson Alexandrino <nasson.alexander@gmail.com>
2019-08-25 20:40:42 +0200

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+List<String> images = [
+  'assets/img_01.jpg',
+  'assets/img_02.jpg',
+  'assets/img_03.jpg',
+  'assets/img_04.jpg'
+];
+
+List<String> title = [
+  'A apreciar a praia',
+  'Entrada para o risote',
+  'Caminhando para o rio',
+  'A caminho das ferias'
+];
